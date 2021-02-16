f6131f9d670e4b45864d0c036955337a7467f0c7
Chapman Schleiss <r3naissance@users.noreply.github.com>
2021-02-16 11:15:00 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd /opt
 
 echo "Installing tools via APT"
 apt update
-apt install -y openvpn golang brutespary nmap xsltproc sslscan python3-pip tmux parallel ike-scan
+apt install python3-pip openvpn golang brutespray nmap xsltproc sslscan tmux parallel ike-scan -y
 
 echo "Installing tools via PIP"
 python3 -m pip install sslyze trufflehog
@@ -19,42 +19,48 @@ echo "Getting nmap bootstrap"
 git clone https://github.com/honze-net/nmap-bootstrap-xsl
 
 echo "Installing cors scanner"
-git clone https://github.com/chenjj/CORScanner
-python3 -m pip install -r requirements.txt
-cors_scan.py -h >> /tmp/install.log
+git clone https://github.com/chenjj/CORScanner cors
+python3 -m pip install -r cors/requirements.txt
+python3 /opt/cors/cors_scan.py -h
 
 echo "Installing gospider"
 git clone https://github.com/jaeles-project/gospider
+cd gospider
 go build .
 mv gospider /usr/local/bin/
-gospider -h >> /tmp/install.log
+gospider -h
 
 echo "Installing dalfox"
+cd /opt
 git clone https://github.com/hahwul/dalfox
+cd dalfox
 go build .
 mv dalfox /usr/local/bin/
-dalfox -h >> /tmp/install.log
+dalfox -h
 
 echo "Installing subfinder"
+cd /opt
 git clone https://github.com/projectdiscovery/subfinder.git
 cd subfinder/v2/cmd/subfinder
 go build .
 mv subfinder /usr/local/bin/
-subfinder -h >> /tmp/install.log
+subfinder -h
 
 echo "Installing nuclei"
+cd /opt
 git clone https://github.com/projectdiscovery/nuclei.git
 cd nuclei/v2/cmd/nuclei
 go build .
 mv nuclei /usr/local/bin/
-nuclei -version >> /tmp/install.log
+nuclei -version
 
 echo "Installing httpx"
+cd /opt
 git clone https://github.com/projectdiscovery/httpx.git
 cd httpx/cmd/httpx
 go build .
 mv httpx /usr/local/bin/
-httpx -version >> /tmp/install.log
+httpx -version
 
 echo "Updating everything and cleaning up"
 apt dist-upgrade -y
