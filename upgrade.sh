0ea80a66499107893b6dca9ed23fe54c7085966d
r3naissance <r3naissance@users.noreply.github.com>
2021-09-11 12:07:19 -0600

    Rebase

--- upgrade.sh
+++ upgrade.sh
@@ -4,34 +4,41 @@ git config pull.rebase true
 
 echo "Getting SecLists"
 cd /opt/SecLists
+git config pull.rebase true
 git pull
 
 echo "Getting sqlmap"
 cd /opt/sqlmap
+git config pull.rebase true
 git pull
 
 echo "Getting NSE"
 cd /opt/nse
+git config pull.rebase true
 git pull
 
 echo "Getting nmap bootstrap"
 cd /opt/nmap-bootstrap-xsl
+git config pull.rebase true
 git pull
 
 echo "Installing cors scanner"
 cd /opt/cors
+git config pull.rebase true
 git pull
 python3 -m pip install -r requirements.txt
 python3 /opt/cors/cors_scan.py -h
 
 echo "Installing hakrawler"
 cd /opt/hakrawler
+git config pull.rebase true
 git pull
 go build .
 mv hakrawler /usr/local/bin/
 
 echo "Installing gospider"
 cd /opt/gospider
+git config pull.rebase true
 git pull
 go build .
 mv gospider /usr/local/bin/
@@ -39,6 +46,7 @@ gospider -h
 
 echo "Installing dalfox"
 cd /opt/dalfox
+git config pull.rebase true
 git pull
 go build .
 mv dalfox /usr/local/bin/
@@ -46,6 +54,7 @@ dalfox -h
 
 echo "Installing subfinder"
 cd /opt/subfinder
+git config pull.rebase true
 git pull
 cd v2/cmd/subfinder
 go build .
@@ -54,6 +63,7 @@ subfinder -h
 
 echo "Installing nuclei"
 cd /opt/nuclei
+git config pull.rebase true
 git pull
 cd v2/cmd/nuclei
 go build .
@@ -62,6 +72,7 @@ nuclei -version
 
 echo "Installing httpx"
 cd /opt/httpx
+git config pull.rebase true
 git pull
 cd cmd/httpx
 go build .
