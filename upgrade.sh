5e7fb717d01967ac0b6f0d1cdf309209ed90cd43
r3naissance <r3naissance@users.noreply.github.com>
2021-09-27 13:37:44 -0600

    Added logic

--- upgrade.sh
+++ upgrade.sh
@@ -1,83 +1,136 @@
 #!/bin/bash
 
-git config pull.rebase true
-
-echo "Getting SecLists"
+echo -n "Getting SecLists "
 cd /opt/SecLists
 git config pull.rebase true
-git pull
+gp=$(git pull)
+if [[ ! $gp == 'Already up to date.' ]]; then
+  echo $gp
+else
+  echo "[DONE]"
+fi
 
-echo "Getting sqlmap"
+echo -n "Getting sqlmap "
 cd /opt/sqlmap
 git config pull.rebase true
-git pull
+gp=$(git pull)
+if [[ ! $gp == 'Already up to date.' ]]; then
+  echo $gp
+else
+  echo "[DONE]"
+fi
 
-echo "Getting NSE"
+echo -n "Getting NSE "
 cd /opt/nse
 git config pull.rebase true
-git pull
+gp=$(git pull)
+if [[ ! $gp == 'Already up to date.' ]]; then
+  echo $gp
+else
+  echo "[DONE]"
+fi
 
-echo "Getting nmap bootstrap"
+echo -n "Getting nmap bootstrap "
 cd /opt/nmap-bootstrap-xsl
 git config pull.rebase true
-git pull
+gp=$(git pull)
+if [[ ! $gp == 'Already up to date.' ]]; then
+  echo $gp
+else
+  echo "[DONE]"
+fi
 
-echo "Installing cors scanner"
+echo -n "Installing cors scanner "
 cd /opt/cors
 git config pull.rebase true
-git pull
-python3 -m pip install -r requirements.txt
-python3 /opt/cors/cors_scan.py -h
+gp=$(git pull)
+if [[ ! $gp == 'Already up to date.' ]]; then
+  echo $gp
+  python3 -m pip install -r requirements.txt
+  python3 /opt/cors/cors_scan.py -h
+else
+  echo "[DONE]"
+fi
 
-echo "Installing hakrawler"
+echo -n "Installing hakrawler "
 cd /opt/hakrawler
 git config pull.rebase true
-git pull
-go build .
-mv hakrawler /usr/local/bin/
+gp=$(git pull)
+if [[ ! $gp == 'Already up to date.' ]]; then
+  echo $gp
+  go build .
+  mv hakrawler /usr/local/bin/
+else
+  echo "[DONE]"
+fi
 
-echo "Installing gospider"
+echo -n "Installing gospider "
 cd /opt/gospider
 git config pull.rebase true
-git pull
-go build .
-mv gospider /usr/local/bin/
-gospider -h
+gp=$(git pull)
+if [[ ! $gp == 'Already up to date.' ]]; then
+  echo $gp
+  go build .
+  mv gospider /usr/local/bin/
+  gospider -h
+else
+  echo "[DONE]"
+fi
 
-echo "Installing dalfox"
+echo -n "Installing dalfox "
 cd /opt/dalfox
 git config pull.rebase true
-git pull
-go build .
-mv dalfox /usr/local/bin/
-dalfox -h
+gp=$(git pull)
+if [[ ! $gp == 'Already up to date.' ]]; then
+  echo $gp
+  go build .
+  mv dalfox /usr/local/bin/
+  dalfox -h
+else
+  echo "[DONE]"
+fi
 
-echo "Installing subfinder"
+echo -n "Installing subfinder "
 cd /opt/subfinder
 git config pull.rebase true
-git pull
-cd v2/cmd/subfinder
-go build .
-mv subfinder /usr/local/bin/
-subfinder -h
+gp=$(git pull)
+if [[ ! $gp == 'Already up to date.' ]]; then
+  echo $gp
+  cd v2/cmd/subfinder
+  go build .
+  mv subfinder /usr/local/bin/
+  subfinder -h
+else
+  echo "[DONE]"
+fi
 
-echo "Installing nuclei"
+echo -n "Installing nuclei "
 cd /opt/nuclei
 git config pull.rebase true
-git pull
-cd v2/cmd/nuclei
-go build .
-mv nuclei /usr/local/bin/
-nuclei -version
+gp=$(git pull)
+if [[ ! $gp == 'Already up to date.' ]]; then
+  echo $gp
+  cd v2/cmd/nuclei
+  go build .
+  mv nuclei /usr/local/bin/
+  nuclei -version
+else
+  echo "[DONE]"
+fi
 
-echo "Installing httpx"
+echo -n "Installing httpx "
 cd /opt/httpx
 git config pull.rebase true
-git pull
-cd cmd/httpx
-go build .
-mv httpx /usr/local/bin/
-httpx -version
+gp=$(git pull)
+if [[ ! $gp == 'Already up to date.' ]]; then
+  echo $gp
+  cd cmd/httpx
+  go build .
+  mv httpx /usr/local/bin/
+  httpx -version
+else
+  echo "[DONE]"
+fi
 
 echo "Updating everything and cleaning up"
 apt update
