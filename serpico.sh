e7889680652208ac3bc0c68aac4c63e931460d36
r3naissance <r3naissance@users.noreply.github.com>
2022-11-02 14:13:38 -0600

    Update serpico.sh

--- serpico.sh
+++ serpico.sh
@@ -1,16 +1,17 @@
 echo "Removing included docker"
-apt remove docker docker-engine docker.io containerd runc -y
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
 
 echo "Installing dependencies"
-apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+sudo apt-get install ca-certificates curl gnupg lsb-release-y
 
 echo "Adding docker repo"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt update
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
 
 echo "Installing docker from repo"
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 echo "Cloning serpico source"
 cd /opt
