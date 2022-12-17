369d450b76baa3205400e3d0fb6edfd1d2487b38
r3naissance <r3naissance@users.noreply.github.com>
2022-12-16 17:34:41 -0700

    Update cuda.sh

--- cuda.sh
+++ cuda.sh
@@ -1,3 +1,4 @@
+# https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=deb_network
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
